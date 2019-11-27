ea0940ab93df93fc17ace3c9d33df2817ba0ccde
whuGaoXu <15827285378@163.com>
2019-11-27 09:02:56 +0800

    schedule commit

---
+++ src/main/java/Schedule.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+/**
+ * 1.新的url
+ * 2.时间到了的
+ * 3.重要性排序
+ * 4.根据爬虫能力进行选择
+ * 选取逻辑：
+ * 第一步：设置爬取能力，即每天可以爬多少数据量
+ * 第二步：设置比例分成，哪个不够，另外一个则可以补齐
+ * 第三步：新旧分别选取
+ * */
+
+public class Schedule {
+
+
+    private int crawlAbility = 1000; // 每天的爬取能力，也就是24h的能够爬取多少数据
+    private double newProportion = 0.5; // 默认新的占比，
+    private double oldProportion = 0.5;// 旧的占比
+
+    public void init(int crawlAbility, double newProportion){
+        this.crawlAbility = crawlAbility;
+        this.newProportion = newProportion;
+        this.oldProportion = 1 - this.newProportion;
+    }
+
+
+    public List<UrlOnceData> getNewUrls(List<UrlOnceData> urlsOnceData){
+        List<UrlOnceData> newUrls = new ArrayList<UrlOnceData>();
+        for(UrlOnceData tmp : urlsOnceData){
+            if(tmp.getId()==-1){
+                newUrls.add(tmp);
+            }
+        }
+        return newUrls;
+    }
+
+    public List<UrlOnceData> getOldUrls(List<UrlOnceData> urlsOnceData){
+        List<UrlOnceData> oldUrls = new ArrayList<UrlOnceData>();
+        for(UrlOnceData tmp : urlsOnceData){
+            if(tmp.getId()!=-1){
+                oldUrls.add(tmp);
+            }
+        }
+        Collections.sort(oldUrls);
+        return oldUrls;
+    }
+
+
+    public List<UrlOnceData> getWaitCrawlUrls(List<UrlOnceData> oldUrls, List<UrlOnceData> newUrls){
+
+        List<UrlOnceData> waitCrawlUrls = new ArrayList<UrlOnceData>();
+
+        int newUrlsSize = 0;
+        int oldUrlsSize = 0;
+        int newUrlsCrawlAbility = (int) (crawlAbility*newProportion);
+        int oldUrlsCrawlAbility = (int) (crawlAbility*oldProportion);
+        if(newUrls.size()>=newUrlsCrawlAbility && oldUrls.size()>=oldUrlsCrawlAbility){
+            newUrlsSize = newUrlsCrawlAbility;
+            oldUrlsSize = oldUrlsCrawlAbility;
+        }else if(newUrls.size()<newUrlsCrawlAbility && oldUrls.size()>=oldUrlsCrawlAbility){
+            newUrlsSize = newUrlsCrawlAbility;
+            oldUrlsSize = oldUrlsCrawlAbility + newUrlsCrawlAbility - newUrls.size();
+        }else if(newUrls.size()>=newUrlsCrawlAbility && oldUrls.size()<oldUrlsCrawlAbility){
+            newUrlsSize = newUrlsCrawlAbility + oldUrlsCrawlAbility - oldUrls.size();
+            oldUrlsSize = oldUrlsCrawlAbility;
+        }else{
+            newUrlsSize = newUrlsCrawlAbility;
+            oldUrlsSize = oldUrlsCrawlAbility;
+        }
+
+        for(int i=0; i<newUrlsSize; ++i){
+            waitCrawlUrls.add(newUrls.get(i));
+        }
+
+        for(int i=0; i<oldUrlsSize; ++i){
+            waitCrawlUrls.add(oldUrls.get(i));
+        }
+
+        return waitCrawlUrls;
+    }
+
+
+
+
+
+}
+
--- src/main/java/UrlOnceData.java
+++ src/main/java/UrlOnceData.java
@@ -1,4 +1,6 @@
-public class UrlOnceData {
+
+
+public class UrlOnceData implements  Comparable<UrlOnceData> {
 
 
 
@@ -6,7 +8,7 @@ public class UrlOnceData {
     private String url;
     private long fetchTime;
     private long refreshCycle; // 暂定以天为单位
-    private byte[] html;
+    private String html;
 
 
     public int getId() {
@@ -41,14 +43,27 @@ public class UrlOnceData {
         this.refreshCycle = refreshCycle;
     }
 
-    public byte[] getHtml() {
+    public String getHtml() {
         return html;
     }
 
-    public void setHtml(byte[] html) {
+    public void setHtml(String html) {
         this.html = html;
     }
 
-
-
+    @Override
+    public int compareTo(UrlOnceData urlOnceData) {
+        int compareValue = 0;//后续这里可以扩展为点击数，即重要性
+        if(compareValue==0){
+            long fetchTimeDiff = this.getFetchTime() - urlOnceData.getFetchTime();
+            if(fetchTimeDiff>0) {
+                compareValue = 1;
+            }else if(fetchTimeDiff==0){
+                compareValue = 0;
+            }else {
+                compareValue = -1;
+            }
+        }
+        return compareValue;
+    }
 }
